c0ed64b7db8250b8a1e0056b0aca08435b8a8e37
Bum_Bam <53340829+timurrozmetow@users.noreply.github.com>
2025-12-03 16:45:59 +0500

    Update Main.jsx

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -19,10 +19,10 @@ export default function Main() {
           <AboutSection />
           <Slider />
           
-      <News />
+      {/* <News /> */}
       <PhotoGallery />
       <ContactForm />
-      <FAQ />
+      {/* <FAQ /> */}
     </>
   )
 }
